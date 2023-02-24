adab9d45819682e003c6f6dd7e110bd831135714
xnatalienguyen <121984393+xnatalienguyen@users.noreply.github.com>
2023-02-23 21:32:08 -0800

    Update Assignment_2.py
    Multiplication Overload and Test

--- Assignment_2.py
+++ Assignment_2.py
@@ -18,7 +18,9 @@ class SignalDetection:
         return c
     def __add__(self, other):
         return SignalDetection(self.hit + other.hit, self.misses + other.misses, self.falseAlarm + other.falseAlarm, self.correctRejections + other.correctRejections)
-
+    def __mul__(self, scalar):
+        return SignalDetection(self.hit * scalar, self.misses * scalar, self.falseAlarm * scalar, self.correctRejections * scalar)
+        
 import unittest 
 
 class TestSignalDetection(unittest.TestCase):
@@ -51,6 +53,12 @@ class TestSignalDetection(unittest.TestCase):
     def test_addition(self):
         sd = SignalDetection(1, 1, 2, 1) + SignalDetection(2, 1, 1, 3)
         expected = SignalDetection(3, 2, 3, 4).criterion()
+        obtained = sd.criterion()
+        # Compare calculated and expected criterion
+        self.assertEqual(obtained, expected)
+    def test_multiplication(self):
+        sd = SignalDetection(1, 2, 3, 1) * 4
+        expected = SignalDetection(4, 8, 12, 4).criterion()
         obtained = sd.criterion()
         # Compare calculated and expected criterion
         self.assertEqual(obtained, expected)
